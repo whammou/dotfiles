44033ebdc2eda09ac5492e0c52225263ec9ba6fc
whammou <khoabui.ld99@gmail.com>
2024-06-27 20:45:19 +0700

    fixed pacman_install keybind

---
+++ fish/.config/fish/functions/_pacman_install.fish
@@ -0,0 +1,16 @@
+function _pacman_install
+    set -f listed_packages (
+    pacman -Slq | 
+    _fzf_wrapper \
+        --multi \
+        --ansi \
+        --prompt="Packages> " \
+        --preview 'pacman -Si {1}'
+    )
+    if test $status -eq 0
+        commandline --replace -- 'sudo pacman -S '$listed_packages
+    end
+
+commandline --function repaint
+
+end
