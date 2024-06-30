5a98b41ee92359a52aa28fa3e8c0fffa4ed24ac1
whammou <khoabui.ld99@gmail.com>
2024-06-30 21:02:18 +0700

    Added _reset function and Removed _pacman_install

---
+++ fish/.config/fish/functions/_reset.fish
@@ -0,0 +1,4 @@
+function _reset
+    reset; echo ;fastfetch; echo \n
+    commandline --function repaint
+end
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,9 @@
 function fish_user_key_bindings
-    fish_vi_key_bindings --no-erase default
-    bind --mode default \ci _pacman_install
-    bind --mode default \e\ci _paru_install
+    fish_vi_key_bindings default
+
+    for mode in default insert
+        bind --mode $mode \cs _paru_install
+        bind --mode $mode \co _reset
+    end
 end
 
