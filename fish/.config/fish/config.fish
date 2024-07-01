eba37c099e3298b78d391e599b855f50d16659b3
whammou <khoabui.ld99@gmail.com>
2024-07-02 03:51:37 +0700

    removed aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,22 +28,6 @@ export VMUX_GLOBAL=1
 export VMUX_NOT_SELECT_PANE=2
 
 # alias for commands:
-alias fetch="echo; neofetch --ascii_distro arch_small --ascii_colors 6 6 --colors 5 8 10 6 8 8"
-alias yt='ytfzf --detach -c youtube -stl'
-alias livetex='mkdir .aux/; latexmk --pvc --auxdir=.aux/ --emulate-aux-dir -recorder- --pdf'
-alias grip="grip --render-math --user-content --quiet"
-alias tt="tt -theme mine"
-alias NOTE="clear; cd /home/whammou/notes; git status"
-alias tree="tree -C"
-alias scim="sc-im"
-alias mdgrep='grep -rin --color=always --include="*.md*"'
-alias less="less -R"
-alias apt="nala"
-alias syncthing="syncthing --no-browser"
-alias ls="ls --group-directories-first --sort=extension --color=always"
-alias python='python3'
-alias calc="python3 -i ~/.myscript/calc.py"
-alias rm="rm -i"
-
-export LANG=en_US.UTF-8
+#alias livetex='mkdir .aux/; latexmk --pvc --auxdir=.aux/ --emulate-aux-dir -recorder- --pdf'
+#export LANG=en_US.UTF-8
 
