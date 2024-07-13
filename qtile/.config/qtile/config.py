cc08187ba7ba61b0273e9b654efb59da8f453b2b
whammou <khoabui.ld99@gmail.com>
2024-07-13 21:21:55 +0700

    Added Bonsai

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,31 +1,6 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from libqtile import bar, layout, qtile, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from qtile_bonsai import Bonsai
+from libqtile.config import Click, Drag, Group, Key,EzKey, KeyChord, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
@@ -33,51 +8,185 @@ mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
+    
+    # Open your terminal emulator quickly. See further below for how to
+    # directly open other apps as splits/tabs using something like rofi.
+    EzKey("M-v", lazy.layout.spawn_split(terminal, "x")),
+    EzKey("M-x", lazy.layout.spawn_split(terminal, "y")),
+    EzKey("M-t", lazy.layout.spawn_tab(terminal)),
+    EzKey("M-S-t", lazy.layout.spawn_tab(terminal, new_level=True)),
+    
+    # Sometimes it's handy to have a split open in the 'previous' position
+    EzKey("M-S-v", lazy.layout.spawn_split(terminal, "x", position="previous")),
+    EzKey("M-S-x", lazy.layout.spawn_split(terminal, "y", position="previous")),
+
+    # Motions to move focus. The names are compatible with built-in layouts.
+    EzKey("M-h", lazy.layout.left()),
+    EzKey("M-l", lazy.layout.right()),
+    EzKey("M-k", lazy.layout.up()),
+    EzKey("M-j", lazy.layout.down()),
+    EzKey("M-d", lazy.layout.prev_tab()),
+    EzKey("M-f", lazy.layout.next_tab()),
+    
+    # Precise motions to move directly to specific tabs at the nearest tab level
+    EzKey("M-1", lazy.layout.focus_nth_tab(1, level=-1)),
+    EzKey("M-2", lazy.layout.focus_nth_tab(2, level=-1)),
+    EzKey("M-3", lazy.layout.focus_nth_tab(3, level=-1)),
+    EzKey("M-4", lazy.layout.focus_nth_tab(4, level=-1)),
+    EzKey("M-5", lazy.layout.focus_nth_tab(5, level=-1)),
+    
+    # Precise motions to move directly to specific tabs at the nearest tab level
+    EzKey("M-S-1", lazy.layout.focus_nth_tab(1, level=1)),
+    EzKey("M-S-2", lazy.layout.focus_nth_tab(2, level=1)),
+    EzKey("M-S-3", lazy.layout.focus_nth_tab(3, level=1)),
+    EzKey("M-S-4", lazy.layout.focus_nth_tab(4, level=1)),
+    EzKey("M-S-5", lazy.layout.focus_nth_tab(5, level=1)),
+
+    # Precise motions to move to specific windows. The options provided here let
+    # us pick the nth window counting only from under currently active [sub]tabs
+    EzKey("C-1", lazy.layout.focus_nth_window(1, ignore_inactive_tabs_at_levels=[1,2])),
+    EzKey("C-2", lazy.layout.focus_nth_window(2, ignore_inactive_tabs_at_levels=[1,2])),
+    EzKey("C-3", lazy.layout.focus_nth_window(3, ignore_inactive_tabs_at_levels=[1,2])),
+    EzKey("C-4", lazy.layout.focus_nth_window(4, ignore_inactive_tabs_at_levels=[1,2])),
+    EzKey("C-5", lazy.layout.focus_nth_window(5, ignore_inactive_tabs_at_levels=[1,2])),
+
+    # Resize operations
+    EzKey("M-C-h", lazy.layout.resize("left", 100)),
+    EzKey("M-C-l", lazy.layout.resize("right", 100)),
+    EzKey("M-C-k", lazy.layout.resize("up", 100)),
+    EzKey("M-C-j", lazy.layout.resize("down", 100)),
+
+    # Swap windows/tabs with neighbors
+    EzKey("M-S-h", lazy.layout.swap("left")),
+    EzKey("M-S-l", lazy.layout.swap("right")),
+    EzKey("M-S-k", lazy.layout.swap("up")),
+    EzKey("M-S-j", lazy.layout.swap("down")),
+    EzKey("A-S-d", lazy.layout.swap_tabs("previous")),
+    EzKey("A-S-f", lazy.layout.swap_tabs("next")),
+    
+    # Manipulate selections after entering container-select mode
+    EzKey("M-o", lazy.layout.select_container_outer()),
+    EzKey("M-i", lazy.layout.select_container_inner()),
+
+# It's kinda nice to have more advanced window management commands under a
+    # qtile key chord.
+    KeyChord(
+        ["mod4"],
+        "w",
+        [
+            # Use something like rofi to pick GUI apps to open as splits/tabs.
+            #EzKey("v", lazy.layout.spawn_split(rofi_run_cmd, "x")),
+            #EzKey("x", lazy.layout.spawn_split(rofi_run_cmd, "y")),
+            #EzKey("t", lazy.layout.spawn_tab(rofi_run_cmd)),
+            #EzKey("S-t", lazy.layout.spawn_tab(rofi_run_cmd, new_level=True)),
+            
+            # Toggle container-selection mode to split/tab over containers of
+            # multiple windows. Manipulate using select_container_outer()/select_container_inner()
+            EzKey("C-v", lazy.layout.toggle_container_select_mode()),
+            
+            EzKey("o", lazy.layout.pull_out()),
+            EzKey("u", lazy.layout.pull_out_to_tab()),
+            
+            EzKey("r", lazy.layout.rename_tab()),
+            
+            # Directional commands to merge windows with their neighbor into subtabs.
+            KeyChord(
+                [],
+                "m",
+                [
+                    EzKey("h", lazy.layout.merge_to_subtab("left")),
+                    EzKey("l", lazy.layout.merge_to_subtab("right")),
+                    EzKey("j", lazy.layout.merge_to_subtab("down")),
+                    EzKey("k", lazy.layout.merge_to_subtab("up")),
+
+                    # Merge entire tabs with each other as splits
+                    EzKey("S-h", lazy.layout.merge_tabs("previous")),
+                    EzKey("S-l", lazy.layout.merge_tabs("next")),
+                ],
+            ),
+            
+            # Directional commands for push_in() to move window inside neighbor space.
+            KeyChord(
+                [],
+                "i",
+                [
+                    EzKey("j", lazy.layout.push_in("down")),
+                    EzKey("k", lazy.layout.push_in("up")),
+                    EzKey("h", lazy.layout.push_in("left")),
+                    EzKey("l", lazy.layout.push_in("right")),
+                    
+                    # It's nice to be able to push directly into the deepest
+                    # neighbor node when desired. The default bindings above
+                    # will have us push into the largest neighbor container.
+                    EzKey(
+                        "S-j",
+                        lazy.layout.push_in("down", dest_selection="mru_deepest"),
+                    ),
+                    EzKey(
+                        "S-k",
+                        lazy.layout.push_in("up", dest_selection="mru_deepest"),
+                    ),
+                    EzKey(
+                        "S-h",
+                        lazy.layout.push_in("left", dest_selection="mru_deepest"),
+                    ),
+                    EzKey(
+                        "S-l",
+                        lazy.layout.push_in("right", dest_selection="mru_deepest"),
+                    ),
+                ],
+            ),
+        ]
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    
+    # Your other bindings
+    # ...
+
+    ## A list of available commands that can be bound to keys can be found
+    ## at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    ## Switch between windows
+    #Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    #Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    #Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    #Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    ## Move windows between left/right columns or move up/down in current stack.
+    ## Moving out of range in Columns layout will create new column.
+    #Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    #Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    #Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    #Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    ## Grow windows. If current window is on the edge of screen and direction
+    ## will be to screen edge - window would shrink.
+    #Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    #Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    #Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    #Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    #Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    ## Toggle between split and unsplit sides of stack.
+    ## Split = all windows displayed
+    ## Unsplit = 1 window displayed, like Max layout, but still with
+    ## multiple stack panes
+    #Key(
+    #    [mod, "shift"],
+    #    "Return",
+    #    lazy.layout.toggle_split(),
+    #    desc="Toggle between split and unsplit sides of stack",
+    #),
+    #Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    ## Toggle between different layouts as defined below
+    #Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    #Key(
+    #    [mod],
+    #    "f",
+    #    lazy.window.toggle_fullscreen(),
+    #    desc="Toggle fullscreen on the focused window",
+    #),
+    #Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    #Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    #Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -94,35 +203,38 @@ for vt in range(1, 8):
     )
 
 
-groups = [Group(i) for i in "123456789"]
-
-for i in groups:
-    keys.extend(
-        [
-            # mod + group number = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod + shift + group number = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+#groups = [Group(i) for i in "123456789"]
+#
+#for i in groups:
+#    keys.extend(
+#        [
+#            # mod + group number = switch to group
+#            Key(
+#                [mod],
+#                i.name,
+#                lazy.group[i.name].toscreen(),
+#                desc="Switch to group {}".format(i.name),
+#            ),
+#            # mod + shift + group number = switch to & move focused window to group
+#            Key(
+#                [mod, "shift"],
+#                i.name,
+#                lazy.window.togroup(i.name, switch_group=True),
+#                desc="Switch to & move focused window to group {}".format(i.name),
+#            ),
+#            # Or, use below if you prefer not to switch to that group.
+#            # # mod + shift + group number = move focused window to group
+#            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+#            #     desc="move focused window to group {}".format(i.name)),
+#        ]
+#    )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
+    Bonsai(**{
+        "window.border_size": 1,
+    }),
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    # layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
