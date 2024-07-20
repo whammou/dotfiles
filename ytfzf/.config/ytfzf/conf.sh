d3778ac80ac04bde8d497208d20b16d6843dd951
whammou <khoabui.ld99@gmail.com>
2024-07-21 02:04:18 +0700

    added default spawn format

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -1,4 +1,6 @@
 notify_playing=1
-invidious_instance='https://invidious.protokolla.fi'
+ytdl_pref='bestvideo[height<=720]+bestaudio/best[height<=720]'
+thumbnail_quality='sddefault'
+invidious_instance='https://inv.nadeko.net'
 #thumbnail_viewer='mpv'
 #search_sort_by='view_count'
