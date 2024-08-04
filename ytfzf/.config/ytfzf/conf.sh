98203871499760a438f52205deda683d1378a9fd
whammou <khoabui.ld99@gmail.com>
2024-08-04 22:44:54 +0700

    Remove instance

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -2,7 +2,7 @@ notify_playing=1
 ytdl_pref='worstvideo[height>=720]+bestaudio'
 ytdl_opts='write--auto-subs'
 thumbnail_quality='sddefault'
-invidious_instance='https://inv.nadeko.net'
+#invidious_instance='https://inv.nadeko.net'
 url_handler_opts='--shuffle'
 #thumbnail_viewer='mpv'
 #search_sort_by='view_count'
