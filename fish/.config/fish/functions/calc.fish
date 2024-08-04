824c14bf9d112f3f373ca0e5907919baed1b246c
whammou <khoabui.ld99@gmail.com>
2024-08-04 22:44:01 +0700

    update fish functions

--- fish/.config/fish/functions/calc.fish
+++
@@ -1,4 +0,0 @@
-function calc --wraps='python3 -i ~/.myscript/calc.py' --description 'alias calc=python3 -i ~/.myscript/calc.py'
-  python3 -i ~/.myscript/calc.py $argv
-        
-end
--- fish/.config/fish/functions/smb-umount.fish
+++ fish/.config/fish/functions/smb-umount.fish
@@ -1,4 +1,4 @@
 function smb-umount --wraps='sudo umount -fl /server' --description 'alias smb-umount=sudo umount -fl /server'
-  sudo umount -fl /server $argv
+  sudo umount -fl /server $argv && qtile cmd-obj -o widget textbox_1 -f update -a " "
         
 end
