3e0c540a7c773b265715de10f6ba5c916410403a
whammou <khoabui.ld99@gmail.com>
2024-08-19 20:44:07 +0700

    minor update

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -1,8 +1,10 @@
 notify_playing=1
 ytdl_pref='worstvideo[height>=720]+bestaudio'
-ytdl_opts='write--auto-subs'
+ytdl_opts='--write-auto-sub --all-subs'
 thumbnail_quality='sddefault'
-#invidious_instance='https://inv.nadeko.net'
+invidious_instance='https://inv.nadeko.net'
 url_handler_opts='--shuffle'
+#max_thread_count=5
+sort_by=relavence,view_count,upload_date
 #thumbnail_viewer='mpv'
 #search_sort_by='view_count'
