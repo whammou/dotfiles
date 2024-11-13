939665cb5fb07ee156c45d0323ca1fa2aece889c
whammou@arch <khoabui.ld99@gmail.com>
2024-11-13 08:52:24 +0700

    minor updates

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -3,7 +3,7 @@ ytdl_pref='worstvideo[height>=720]+bestaudio'
 ytdl_opts='--write-auto-sub --all-subs'
 thumbnail_quality='sddefault'
 #invidious_instance='https://inv.nadeko.net'
-invidious_instance='https://iv.nowhere.moe'
+#invidious_instance='https://iv.nowhere.moe'
 url_handler_opts='--shuffle'
 #max_thread_count=5
 sort_by=relavence,view_count,upload_date
