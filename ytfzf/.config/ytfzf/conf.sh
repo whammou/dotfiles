79cfc23d69e33ccaa5b46e0f6943582d6dad01cf
whammou@arch <khoabui.ld99@gmail.com>
2024-12-15 20:28:22 +0700

    update server adress

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -3,7 +3,7 @@ ytdl_pref='worstvideo[height>=720]+bestaudio'
 ytdl_opts='--write-auto-sub --all-subs'
 thumbnail_quality='sddefault'
 thumbnail_viewer='kitty'
-#invidious_instance='https://inv.nadeko.net'
+invidious_instance='https://inv.nadeko.net'
 #invidious_instance='https://iv.nowhere.moe'
 url_handler_opts='--shuffle'
 #max_thread_count=5
