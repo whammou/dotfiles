bc445f7dfcfcdaee12d863d309a866aaaa719fd4
whammou@arch <khoabui.ld99@gmail.com>
2024-12-15 20:25:11 +0700

    update variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,34 +1,25 @@
 if status is-interactive
+
     # Commands to run in interactive sessions can go here
     starship init fish | source
     # echo; neofetch --ascii_distro arch_small --ascii_colors 6 6 --colors 5 8 10 6 8 8
     echo; fastfetch
     set fish_greeting
-    if string match -q -- '*' $TERM
-        set -g fish_vi_force_cursor 1
-    end
+
+    #    if string match -q -- '*' $TERM
+    #        set -g fish_vi_force_cursor 1
+    #    end
 end
+
 set fish_cursor_default block
 set fish_cursor_insert line
 
 # Term settings
-export TERM=xterm-256color
+#export TERM=xterm-256color
 
 # Fish FZF settings
-set fzf_fd_opts --hidden
 fzf --fish | source
 
 # set Neovim as default text editor
 export VISUAL=nvim
 export EDITOR="$VISUAL"
-
-# vmux customization
-export VMUX_EDITOR=nvim
-export VMUX_NVIM_SESSION_DIR=~/.cache/nvim_sessions
-export VMUX_GLOBAL=1
-export VMUX_NOT_SELECT_PANE=2
-
-# alias for commands:
-#alias livetex='mkdir .aux/; latexmk --pvc --auxdir=.aux/ --emulate-aux-dir -recorder- --pdf'
-#export LANG=en_US.UTF-8
-
