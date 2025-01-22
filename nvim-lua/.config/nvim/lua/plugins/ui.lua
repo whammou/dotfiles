7b9dd19c3222a09488ac4deaef316cd8b54bd0f0
whammou@arch <khoabui.ld99@gmail.com>
2025-01-23 00:56:55 +0700

    added folke/noice

--- nvim-lua/.config/nvim/lua/plugins/ui.lua
+++ nvim-lua/.config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,20 @@
 return { 
   {
-    "rcarriga/nvim-notify",
-    opt = {},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      presets = {
+        long_message_to_split = true,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      { 
+        "rcarriaga/nvim-notify",
+        opts = {
+          stages = "static",
+        },
+      }
+    },
   },
 }
