6f034b73c61866270fadd121c505f72cfddeaa4b
whammou@arch <khoabui.ld99@gmail.com>
2025-01-31 22:39:06 +0700

    add widgets

--- qtile/.config/qtile/settings/widgets.py
+++ qtile/.config/qtile/settings/widgets.py
@@ -1,8 +1,29 @@
-from libqtile import widget
+from qtile_extras import widget
+from qtile_extras.widget.decorations import PowerLineDecoration
+
+
+widgets = [
+    widget.CurrentLayout(),
+    widget.GroupBox(),
+    widget.Prompt(),
+    widget.WindowName(),
+    widget.Chord(
+        chords_colors={
+            "launch": ("#ff0000", "#ffffff"),
+        },
+        name_transform=lambda name: name.upper(),
+    ),
+    widget.TextBox("default config", name="default"),
+    widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+    widget.Systray(),
+    widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+    widget.QuickExit(),
+]
 
 widget_defaults = dict(
     font="sans",
-    fontsize=12,
+    fontsize=16,
     padding=3,
 )
+
 extension_defaults = widget_defaults.copy()
