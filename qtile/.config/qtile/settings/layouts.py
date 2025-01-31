7257b8a032d133152800cf74f1660d8018ecf755
whammou@arch <khoabui.ld99@gmail.com>
2025-01-31 22:39:29 +0700

    add Bonsai

--- qtile/.config/qtile/settings/layouts.py
+++ qtile/.config/qtile/settings/layouts.py
@@ -1,19 +1,27 @@
 from libqtile import layout
 from libqtile.config import Match
+from qtile_bonsai import Bonsai
 
 layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     layout.Max(),
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    Bonsai(**{
+        "window.border_size": 0,
+        "window.margin": [0, 3, 6, 3],
+        "window.default_add_mode": "tab",
+        "container_select_mode.border_color": "ff00ff",
+        "container_select_mode.borser_size": 1,
+        "L1.tab_bar.hide_when": "always",
+        "tab_bar.height": 6,
+        "tab_bar.margin": [0, 3, 0, 3],
+        "tab_bar.tab.font_size": 1,
+        "L2.tab_bar.tab.bg_color": "007676",
+        "L2.tab_bar.tab.active.bg_color": "41a7fc",
+        "L3.tab_bar.tab.bg_color": "500076",
+        "L3.tab_bar.tab.active.bg_color": "c75ae8",
+        #"L4.tab_bar.tab.bg_color": "76002a",
+        #"L4.tab_bar.tab.active.bg_color": "ff005c",
+    }),
 ]
 
 floating_layout = layout.Floating(
