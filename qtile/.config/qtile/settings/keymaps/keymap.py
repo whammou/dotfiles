e573224e216e14f8dc2d34495774e8cf36569943
whammou@arch <khoabui.ld99@gmail.com>
2025-02-09 01:52:07 +0700

    application keymapping

---
+++ qtile/.config/qtile/settings/keymaps/keymap.py
@@ -0,0 +1,36 @@
+cmd_keymaps = [
+    {
+        "name": "qutebrowser",
+        "prefix": "q",
+        "cmd": [
+            ["b", "qb"],
+            ["p", "qb-url"],
+            ["o", "qb_bookmark"],
+        ],
+    },
+    {
+        "name": "lobster",
+        "prefix": "u",
+        "cmd": [
+            ["l", "kitty --hold -e 'lobsterfzf'"],
+            ["S-l", "kitty --hold -e 'lobsterfzf_trending'"],
+        ],
+    },
+    {
+        "name": "orgmode",
+        "prefix": "o",
+        "cmd": [
+            ["a", "kitty -e 'orgmode-agenda'"],
+            ["t", "orgmode-todo"],
+            ["c", "orgmode-capture"],
+        ],
+    },
+    {
+        "name": "yazi",
+        "prefix": "f",
+        "cmd": [
+            ["h", "kitty --hold -e yazi $HOME"],
+            ["s", "kitty --hold -e yazi /server/"],
+        ],
+    },
+]
