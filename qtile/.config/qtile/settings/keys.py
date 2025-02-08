1d7ae0a88ddb6f9185f0b6c49a1ca54e6ef6df3c
whammou@arch <khoabui.ld99@gmail.com>
2025-02-09 01:51:55 +0700

    add spawn_position method

--- qtile/.config/qtile/settings/keys.py
+++ qtile/.config/qtile/settings/keys.py
@@ -1,13 +1,17 @@
 import itertools
 
+from .keymaps.keymap import cmd_keymaps
 from .keymaps.general import general_keybinds
 from .keymaps.layers import focus_visible_window, change_tab_layer
+from .keymaps.spawn import spawn_position
 
 keys = []
 mod = "mod4"
 
 keys = list(itertools.chain(
     general_keybinds,
-    focus_visible_window(9),
-    change_tab_layer(9, 9),
+    focus_visible_window(range(1, 10)),
+    change_tab_layer(range(1, 10), range(1, 10)),
+    spawn_position(cmd_keymaps, "x", "next"),
+    spawn_position(cmd_keymaps, "y", "next")
 ))
