d1b5b691e26473aecf418bd2cdc7097726dcd425
whammou@arch <khoabui.ld99@gmail.com>
2025-02-10 22:09:45 +0700

    add spawn_tab()

--- qtile/.config/qtile/settings/keymaps/spawn.py
+++ qtile/.config/qtile/settings/keymaps/spawn.py
@@ -17,6 +17,32 @@ def spawn_position(keymap, orientation, position):
             cmd_bind.append(KeyChord([], p, key_bind))
         #print(cmd_bind)
         prefix_bind.extend(cmd_bind)
-    position_bind = KeyChord([mod], orientation, prefix_bind)
+    match position:
+        case "next":
+            position_bind = KeyChord([mod], orientation, prefix_bind)
+        case "previous":
+            position_bind = KeyChord([mod, "Shift"], orientation, prefix_bind)
+    #print(position_bind)
+    return [position_bind]
+
+
+def spawn_tab(keymap, newlevel=False):
+    prefix_bind = []
+    for package in keymap:
+        cmd_bind = []
+        for p in package["prefix"]:
+            key_bind =[]
+            for c in package["cmd"]:
+                #print(c[0], c[1])
+                key_bind.append(EzKey(c[0], lazy.layout.spawn_tab(c[1], new_level=newlevel)))
+            #print(p, key_bind)
+            cmd_bind.append(KeyChord([], p, key_bind))
+        #print(cmd_bind)
+        prefix_bind.extend(cmd_bind)
+    match newlevel:
+        case False:
+            position_bind = KeyChord([mod], "t", prefix_bind)
+        case True:
+            position_bind = KeyChord([mod, "Shift"], "t", prefix_bind)
     #print(position_bind)
     return [position_bind]
