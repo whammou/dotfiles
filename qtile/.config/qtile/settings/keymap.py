e6f6b6b4624b58b9a6fdde1f9c723d5bcf6d965a
whammou@arch <khoabui.ld99@gmail.com>
2025-02-16 05:43:48 +0700

    add method to run script via terminal

--- qtile/.config/qtile/settings/keymap.py
+++ qtile/.config/qtile/settings/keymap.py
@@ -1,4 +1,5 @@
 from .keymaps.spawn import tmux_session_attach
+from .path import script_path, run_script, in_terminal
 
 
 keymap = [
@@ -6,39 +7,55 @@ keymap = [
         "name": "qutebrowser",
         "prefix": "q",
         "cmd": [
-            ["b", "qb"],
-            ["p", "qb-url"],
-            ["o", "qb_bookmark"],
-        ],
-    },
-    {
-        "name": "lobster",
-        "prefix": "u",
-        "cmd": [
-            ["l", "kitty --hold -e 'lobsterfzf'"],
-            ["S-l", "kitty --hold -e 'lobsterfzf_trending'"],
+            ["b",   "qb"],
+            ["p",   "qb-url"],
+            ["S-p", "qutebrowser -T -C /home/whammou/.config/qutebrowser/config.py"]
         ],
     },
     {
         "name": "orgmode",
         "prefix": "o",
         "cmd": [
-            ["a", "kitty -e 'orgmode-agenda'"],
-            ["t", "orgmode-todo"],
-            ["c", "orgmode-capture"],
+            ["a", in_terminal("orgmode-agenda")],
+            ["t", in_terminal("orgmode-todo")],
+            ["c", in_terminal("orgmode-capture")],
         ],
     },
     {
         "name": "yazi",
         "prefix": "f",
         "cmd": [
-            ["h", "kitty --hold -e yazi $HOME"],
-            ["s", "kitty --hold -e yazi /server/"],
+            ["h", in_terminal("yazi $HOME")],
+            ["s", in_terminal("yazi /server/")],
         ],
     },
     {
         "name": "tmux",
         "prefix": "t",
         "cmd": tmux_session_attach(range(1, 10)),
+    },
+    {
+        "name": "monitor",
+        "prefix": "m",
+        "cmd": [
+            ["p", in_terminal("btm")],
+            ["b", in_terminal("monitor-battery")],
+            ["v", in_terminal("monitor-voltage")],
+            ["c", in_terminal("watch-cpu")],
+        ],
+    },
+    {
+        "name": "utility",
+        "prefix": "u",
+        "cmd": [
+            ["a",   in_terminal("anifzf")],
+            ["t",   in_terminal("tt --theme=mine --multi --nohighlight", parameters="-o font_size=20")],
+            ["l",   in_terminal("lobsterfzf")],
+            ["S-l", in_terminal("lobsterfzf")],
+            ["c",   in_terminal(".venv/calculator/bin/python -i /usr/local/bin/calc")],
+            ["m",   in_terminal("ytfzf-music")],
+            ["y",   in_terminal("ytfzf-video")],
+            ["S-y", in_terminal("ytfzf-thumbnail")],
+        ],
     },
 ]
