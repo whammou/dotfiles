53e19e3b4f2d968908e39e57c18ec9a97599c7e0
whammou@arch <khoabui.ld99@gmail.com>
2025-02-16 05:44:21 +0700

    add path to scripts

--- qtile/.config/qtile/settings/path.py
+++ qtile/.config/qtile/settings/path.py
@@ -1,11 +1,19 @@
-
 from os import path
+
 
 qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
 script_path = path.join(qtile_path, "scripts")
 wallpaper_path = path.join(path.expanduser('~'), ".wallpaper")
 
 
-def run_script():
-    return
+def run_script(script_name):
+    return path.join(script_path, script_name)
 
+
+def in_terminal(package, terminal="kitty", parameters=None):
+    package_launch = "-e " + package
+
+    if parameters == None:
+        return " ".join([terminal, package_launch])
+    else:
+        return " ".join([terminal, parameters, package_launch])
