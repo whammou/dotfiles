b9ecdf9fe7bf839d2d4c563e5a10e0621f33007b
whammou@arch <khoabui.ld99@gmail.com>
2025-03-02 21:40:54 +0700

    change snippets location

--- nvim-lazy/.config/nvim/lua/plugins/coding.lua
+++ nvim-lazy/.config/nvim/lua/plugins/coding.lua
@@ -1,10 +1,15 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    config = function()
-      require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/LuaSnip/" } })
-    end,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/snippets/" } })
+      end,
+    },
   },
+
   {
     "saghen/blink.cmp",
     opts = {
