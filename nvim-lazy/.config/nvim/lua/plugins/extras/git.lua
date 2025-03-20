84b0eaeb042df1e3f5ccfc8704256d903968a0bb
whammou@arch <khoabui.ld99@gmail.com>
2025-03-20 07:09:23 +0700

    added lazygit and removed fugitive

--- nvim-lazy/.config/nvim/lua/plugins/extras/git.lua
+++ nvim-lazy/.config/nvim/lua/plugins/extras/git.lua
@@ -1,11 +1,31 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-  },
+  -- {
+  --   "tpope/vim-fugitive",
+  --   lazy = false,
+  -- },
 
   {
     "knsh14/vim-github-link",
     event = "VeryLazy",
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    -- },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    --keys = {
+    --  { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    --},
   },
 }
