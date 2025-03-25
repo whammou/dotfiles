19bf98981d4b0a750456b791d7b5040168a57be0
whammou@arch <khoabui.ld99@gmail.com>
2025-03-25 09:04:51 +0700

    update org_meta_return keybind
    bind org_meta_return to Shift+return

--- nvim-lazy/.config/nvim/lua/config/autocmds.lua
+++ nvim-lazy/.config/nvim/lua/config/autocmds.lua
@@ -3,6 +3,16 @@
 --
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
---
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "org",
+  callback = function()
+    vim.keymap.set("i", "<S-CR>", '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+      silent = true,
+      buffer = true,
+    })
+  end,
+})
+
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
