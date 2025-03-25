4516fed9fdda1df4c8d0a0fbfdd81a918fcd72fd
whammou@arch <khoabui.ld99@gmail.com>
2025-03-25 09:03:44 +0700

    update git keybind
    replace fug with _lazygit for key \cg

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,7 +5,7 @@ function fish_user_key_bindings
         bind --mode $mode \e\cp _paru_install
         bind --mode $mode \e\cq _paru_uninstall
         bind --mode $mode \co _reset
-        bind --mode $mode \cg fug
+        bind --mode $mode \cg _lazygit
 
         bind --mode $mode \cZ true #unbind <C-z> send to background
         bind --mode $mode \x1c bg
