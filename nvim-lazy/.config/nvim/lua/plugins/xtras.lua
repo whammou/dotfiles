accac43afffe37b993018465682a685eee27ae5d
whammou@arch <khoabui.ld99@gmail.com>
2025-04-01 22:00:53 +0700

    update plugin event

--- nvim-lazy/.config/nvim/lua/plugins/xtras.lua
+++ nvim-lazy/.config/nvim/lua/plugins/xtras.lua
@@ -1,11 +1,14 @@
 return {
-  {
-    "norcalli/nvim-colorizer.lua",
-    event = "VeryLazy",
-    opts = {},
-  },
+
+  --  {
+  --    "norcalli/nvim-colorizer.lua",
+  --    event = "VeryLazy",
+  --    opts = {},
+  --  },
+
   {
     "pysan3/fcitx5.nvim",
+    -- lazy = true,
     event = "VeryLazy",
     opts = {},
   },
