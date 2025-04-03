b5efb376debf4f263f65e983bde648689d7dd80c
whammou@arch <khoabui.ld99@gmail.com>
2025-04-03 13:42:32 +0700

    update instances list

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -1,23 +1,27 @@
 notify_playing=1
+max_thread_count=5
+
 ytdl_pref='worstvideo[height>=720]+bestaudio'
+#ytdl_opts='--write-auto-sub --all-subs'
+url_handler_opts='--shuffle'
+#sort_by=relavence,view_count,upload_date
+#search_sort_by='view_count'
+
 thumbnail_quality='sddefault'
 thumbnail_viewer='kitty'
-url_handler_opts='--shuffle'
-sort_by=relavence,view_count,upload_date
-
-max_thread_count=5
 #thumbnail_viewer='mpv'
-#search_sort_by='view_count'
-#ytdl_opts='--write-auto-sub --all-subs'
 
-#invidious_instance='https://inv.nadeko.net'
-#invidious_instance='https://invidious.nerdvpn.de'
-#invidious_instance='https://iv.nowhere.moe'
-#invidious_instance='https://vid.puffyan.us'
-invidious_instance='https://cal1.iv.ggtyler.dev'
-invidious_instance='https://nyc1.iv.ggtyler.dev'
-invidious_instance='https://pol1.iv.ggtyler.dev'
-#invidious_instance='https://vid.lilay.dev'
-#invidious_instance='https://invidious.io'
-#invidious_instance='https://yewtu.be'
-#invidious_instance='https://invidious.jing.rocks'
+#invidious_instance='invidious.nerdvpn.de'
+#invidious_instance='iv.nowhere.moe'
+#invidious_instance='vid.puffyan.us'
+#invidious_instance='vid.lilay.dev'
+#invidious_instance='invidious.io'
+#invidious_instance='yewtu.be'
+#invidious_instance='invidious.jing.rocks'
+
+#invidious_instance='inv5.nadeko.net'
+#invidious_instance="Tor: inv.nadekonw7plitnjuawu6ytjsl7jlglk2t6pyq6eftptmiv3dvqndwvyd.onion"
+
+#invidious_instance='pol1.iv.ggtyler.dev'
+invidious_instance='cal1.iv.ggtyler.dev'
+#invidious_instance='nyc1.iv.ggtyler.dev'
