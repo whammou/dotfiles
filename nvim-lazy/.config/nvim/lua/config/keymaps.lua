969ff5caa43badfdb1daa6414ebb85ffe0665d89
whammou@arch <khoabui.ld99@gmail.com>
2025-04-04 15:53:55 +0700

    remove global keybinds

--- nvim-lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps.lua
@@ -7,10 +7,7 @@ local map = vim.keymap.set
 map("n", "zo", "zMzvzz", { desc = "Unfold only at this level" })
 
 -- Git
-map("n", "<C-g>s", "<cmd>Git | only<cr>", { desc = "Git status" })
 map("n", "<C-g>u", "<cmd>GetCurrentBranchLink<CR>", { desc = "Get current branch link" })
-map("n", "<C-g>f", "<cmd>Git fetch<cr>", { desc = "Git fetch" })
-map("n", "<C-G>p", "<cmd>Git pull<cr>", { desc = "Git pull" })
 
 -- Edit
 map("n", "<C-A-j>", "<cmd>set paste<CR>m`o<ESC>``<cmd>set nopaste<CR>", { desc = "Add empty line above" })
