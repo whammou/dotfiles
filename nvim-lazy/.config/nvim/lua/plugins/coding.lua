eba5f46925644ac0f3a3dae54d0299d7a16443aa
whammou@arch <khoabui.ld99@gmail.com>
2025-04-18 21:55:18 +0700

    successfully source luasnip from blink

--- nvim-lazy/.config/nvim/lua/plugins/coding.lua
+++ nvim-lazy/.config/nvim/lua/plugins/coding.lua
@@ -1,4 +1,21 @@
 return {
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = { "L3MON4D3/LuaSnip", version = "2.*" },
+    opts = {
+      snippets = { preset = "luasnip" },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          snippets = {
+            name = "luasnip",
+          },
+        },
+      },
+    },
+  },
+
   -- {
   --   "L3MON4D3/LuaSnip",
 
@@ -6,22 +23,4 @@ return {
   --     require("luasnip.loaders.from_lua").load({ path = "~/.config/nvim/snippets/" })
   --   end,
   -- },
-
-  {
-    "saghen/blink.cmp",
-    lazy = true,
-
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      lazy = true,
-      opts = {},
-    },
-
-    opts = {
-      snippets = { preset = "luasnip" },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-    },
-  },
 }
