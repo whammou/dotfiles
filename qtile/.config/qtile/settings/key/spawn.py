c2fef113ea4fec84f35c22e377dee80116840e7e
whammou@arch <khoabui.ld99@gmail.com>
2025-04-25 22:41:07 +0700

    added new spawn method
    To avoid spawn in tabbed groups

--- qtile/.config/qtile/settings/key/spawn.py
+++ qtile/.config/qtile/settings/key/spawn.py
@@ -31,6 +31,24 @@ def spawn_position(mod, trigger, keymap, orientation, **spawn):
     return [position_bind]
 
 
+def spawn_new(mod, trigger, keymap, **spawn):
+    prefix_bind = []
+    for package in keymap:
+        cmd_bind = []
+        for p in package["prefix"]:
+            key_bind = []
+            for c in package["cmd"]:
+                # print(c[0], c[1], spawn)
+                key_bind.append(EzKey(c[0], lazy.spawn(c[1], **spawn)))
+            # print(p, key_bind)
+            cmd_bind.append(KeyChord([], p, key_bind))
+        # print(cmd_bind)
+        prefix_bind.extend(cmd_bind)
+    position_bind = KeyChord(mod, trigger, prefix_bind)
+    # print(position_bind)
+    return [position_bind]
+
+
 def spawn_tab(mod, trigger, keymap, **spawn):
     prefix_bind = []
     for package in keymap:
