46f6097fcf18ac36baa61a835e87ff29f331611b
whammou@arch <khoabui.ld99@gmail.com>
2025-04-25 22:40:04 +0700

    disable plugins
    gitsigns
    ts-comments.nvim

--- nvim-lazy/.config/nvim/lua/plugins/disabled.lua
+++ nvim-lazy/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- disable trouble
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  -- { "folke/ts-comments.nvim", enabled = false },
 }
