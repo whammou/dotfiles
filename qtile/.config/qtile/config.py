56ee9ce8c65ed7b2dba894a5ba294a285c2504f0
whammou@arch <khoabui.ld99@gmail.com>
2025-05-04 08:28:31 +0700

    remove focus stealing

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -12,16 +12,21 @@ from os import path
 import subprocess
 
 
-# @hook.subscribe.startup_once
-# def autostart():
-#    subprocess.call([path.join(qtile_path, 'autostart.sh')])
+@hook.subscribe.startup_once
+def autostart():
+    subprocess.call([path.join(qtile_path, "autostart.sh")])
+
+
+@hook.subscribe.client_new
+def prevent_focus_steal(client):
+    client.__class__.can_steal_focus = property(lambda self: False)
 
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
-floats_kept_above = True
+floats_kept_above = False
 cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "never"
