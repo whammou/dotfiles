394581af4ab4340ea521d5810bc9d9f571844506
whammou@arch <khoabui.ld99@gmail.com>
2025-05-05 06:08:57 +0700

    update keybinds

--- qtile/.config/qtile/settings/key/functional.py
+++ qtile/.config/qtile/settings/key/functional.py
@@ -3,6 +3,7 @@ from libqtile.lazy import lazy
 
 
 mod = "mod4"
+alt = "mod1"
 
 functional_keys = [
     Key(
@@ -12,19 +13,19 @@ functional_keys = [
             "pactl set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo toggle"
         ),
     ),
-    Key([mod], "F2", lazy.spawn("volume 1%-")),
-    Key([mod], "F3", lazy.spawn("volume 1%+")),
-    Key([mod], "F4", lazy.spawn("pactl set-source-mute 0 toggle")),
-    Key([mod, "Shift"], "F4", lazy.spawn("noise-supression")),
-    Key([mod], "F5", lazy.spawn("brightness 1%-")),
-    Key([mod], "F6", lazy.spawn("brightness 1%+")),
-    Key([mod], "F7", lazy.spawn("uptime-notification")),
-    Key([mod], "F8", lazy.group["scratchpad"].dropdown_toggle("nmfzf")),
-    Key([mod], "F11", lazy.spawn("vktablet")),
-    Key([mod], "F12", lazy.group["scratchpad"].dropdown_toggle("adapter")),
+    Key([alt], "F2", lazy.spawn("volume 1%-")),
+    Key([alt], "F3", lazy.spawn("volume 1%+")),
+    Key([alt], "F4", lazy.spawn("pactl set-source-mute 0 toggle")),
+    Key([alt, "Shift"], "F4", lazy.spawn("noise-supression")),
+    Key([alt], "F5", lazy.spawn("brightness 1%-")),
+    Key([alt], "F6", lazy.spawn("brightness 1%+")),
+    Key([alt], "F7", lazy.spawn("uptime-notification")),
+    Key([alt], "F8", lazy.group["scratchpad"].dropdown_toggle("nmfzf")),
+    Key([alt], "F11", lazy.spawn("vktablet")),
+    Key([alt], "F12", lazy.group["scratchpad"].dropdown_toggle("adapter")),
     Key([mod], "Space", lazy.spawn("sh /usr/local/bin/toggle-trackpoint")),
-    Key([mod], "Delete", lazy.group["scratchpad"].dropdown_toggle("powermenu")),
-    Key([mod], "Escape", lazy.spawn("dunstctl close-all")),
+    Key([alt], "Delete", lazy.group["scratchpad"].dropdown_toggle("powermenu")),
+    Key([alt], "Escape", lazy.spawn("dunstctl close-all")),
     KeyChord(
         [mod, "shift"],
         "s",
