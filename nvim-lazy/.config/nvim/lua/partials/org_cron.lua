cdcd1f56bf4861f41a761ed17ae31e0277426214
whammou@arch <khoabui.ld99@gmail.com>
2025-05-11 18:13:04 +0700

    added orgmode notification
    orgmode notification using nvim-orgmode and manage with dunst

---
+++ nvim-lazy/.config/nvim/lua/partials/org_cron.lua
@@ -0,0 +1,13 @@
+local orgmode = vim.fn.stdpath("data") .. "/lazy/orgmode"
+vim.opt.runtimepath:append(orgmode)
+-- If you are using Packer or any other package manager that uses built-in package manager, do this:
+--vim.cmd("packadd orgmode")
+
+-- Run the orgmode cron
+require("orgmode").cron({
+  org_agenda_files = "~/notes/**/*",
+  org_default_notes_file = "~/notes/capture.org",
+  notifications = {
+    reminder_time = { 0, 5, 10 },
+  },
+})
--- nvim-lazy/.config/nvim/lua/plugins/extras/orgmode.lua
+++ nvim-lazy/.config/nvim/lua/plugins/extras/orgmode.lua
@@ -67,6 +67,7 @@ return {
         org_return_uses_meta_return = false,
         org = {
           org_cycle = false,
+          org_toggle_checkbox = "<Leader>oc",
         },
       },
 
@@ -77,6 +78,59 @@ return {
         folds = {
           colored = false,
         },
+      },
+      notifications = {
+        enabled = false,
+        cron_enabled = true,
+        repeater_reminder_time = false,
+        deadline_warning_reminder_time = false,
+        deadline_reminder = true,
+        scheduled_reminder = true,
+        notifier = function(tasks)
+          local result = {}
+          for _, task in ipairs(tasks) do
+            require("orgmode.utils").concat(result, {
+              string.format("# %s (%s)", task.category, task.humanized_duration),
+              string.format("%s %s %s", string.rep("*", task.level), task.todo, task.title),
+              string.format("%s: <%s>", task.type, task.time:to_string()),
+            })
+          end
+
+          if not vim.tbl_isempty(result) then
+            require("orgmode.notifications.notification_popup"):new({ content = result })
+          end
+
+          -- Example: if you use Snacks, you can do something like this (THis is not implemented)
+          Snacks.notifier.notify(table.concat(result, "\n"), vim.log.levels.INFO, {
+            title = "Orgmode",
+            ft = "org",
+          })
+        end,
+        cron_notifier = function(tasks)
+          for _, task in ipairs(tasks) do
+            local title = string.format("%s (%s)", task.category, task.humanized_duration)
+            local subtitle = string.format("%s %s %s", string.rep("*", task.level), task.todo, task.title)
+            local date = string.format("%s: %s", task.type, task.time:to_string())
+
+            -- Linux
+            if vim.fn.executable("notify-send") == 1 then
+              vim.system({
+                "notify-send",
+                "-t 0",
+                -- "--icon=~/.local/share/nvim/lazy/orgmode/assets/nvim-orgmode-small.png",
+                "icon=''",
+                "--app-name=orgmode",
+                title,
+                string.format("%s\n%s", subtitle, date),
+              })
+            end
+
+            -- MacOS
+            if vim.fn.executable("terminal-notifier") == 1 then
+              vim.system({ "terminal-notifier", "-title", title, "-subtitle", subtitle, "-message", date })
+            end
+          end
+        end,
       },
     },
   },
