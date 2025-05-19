3c6b9e4192816c0e299acd6f7cd0bb89458093e2
whammou@arch <khoabui.ld99@gmail.com>
2025-05-20 00:41:11 +0700

    update markdown folding method

--- nvim-lazy/.config/nvim/after/ftplugin/markdown.lua
+++ nvim-lazy/.config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,30 @@
 local opt = vim.opt
 
-opt.foldlevel = 99
+function _G.markdown_foldexpr()
+  local lnum = vim.v.lnum
+  local line = vim.fn.getline(lnum)
+  local heading = line:match("^(#+)%s")
+  if heading then
+    local level = #heading
+    if level == 1 then
+      -- Special handling for H1
+      if lnum == 1 then
+        return ">1"
+      else
+        local frontmatter_end = vim.b.frontmatter_end
+        if frontmatter_end and (lnum == frontmatter_end + 1) then
+          return ">1"
+        end
+      end
+    elseif level >= 2 and level <= 6 then
+      -- Regular handling for H2-H6
+      return ">" .. level
+    end
+  end
+  return "="
+end
+
+opt.foldlevel = 0
 opt.foldmethod = "expr"
-opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+opt.foldexpr = "v:lua.markdown_foldexpr()"
+-- opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
