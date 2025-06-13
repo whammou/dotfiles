46ed1cf0af5fc2097a704d05111a98f14f08f2d7
whammou@arch <khoabui.ld99@gmail.com>
2025-06-13 14:04:42 +0700

    update keybinds
    integrate fcitx5 to qutebrowser

--- qutebrowser/.config/qutebrowser/keys.py
+++ qutebrowser/.config/qutebrowser/keys.py
@@ -9,6 +9,32 @@ config.unbind("d")
 config.unbind("D")
 config.unbind("F")
 
+# Input method
+# config.bind("<Escape>", "spawn fcitx5-remote -s keyboard-us", mode="insert")
+# ~/.config/qutebrowser/config.py
+
+
+# Bind Escape in insert mode to switch Fcitx5 and then leave insert mode
+# This might require two presses or careful timing, as qutebrowser's own
+# Escape handling might still fire.
+config.bind(
+    "<Escape>",
+    "spawn fcitx5-remote -s keyboard-us ;; mode-leave",
+    mode="insert",
+)
+
+config.bind(
+    "<Shift-i>", "spawn fcitx5-remote -s unikey ;; mode-enter insert", mode="normal"
+)
+
+# Settings
+config.bind("xb", "config-cycle statusbar.show never always")
+config.bind(
+    "xc",
+    "config-cycle content.user_stylesheets ~/.config/qutebrowser/css/default.css ~/.config/qutebrowser/css/custom.css",
+)
+config.bind("xt", "config-cycle tabs.show multiple always")
+# Qtile
 c.aliases["split"] = "spawn _qtile_spawn_split_y"
 c.aliases["vsplit"] = "spawn _qtile_spawn_split_x"
 c.aliases["tab"] = "spawn _qtile_spawn_tab"
@@ -20,37 +46,28 @@ config.bind("V", "cmd-set-text -s :vsplit", mode="normal")
 config.bind("T", "cmd-set-text -s :tab")
 config.bind("<Ctrl-T>", "cmd-set-text -s :tab_new")
 
-# Qtile
 config.bind(
     "Fv",
-    "hint links spawn qtile cmd-obj -o layout -f spawn_split -a 'qb {hint-url}' x",
+    "hint all spawn qtile cmd-obj -o layout -f spawn_split -a 'xdg-open {hint-url}' x",
 )
 config.bind(
     "Fx",
-    "hint links spawn qtile cmd-obj -o layout -f spawn_split -a 'qb {hint-url}' y",
+    "hint all spawn qtile cmd-obj -o layout -f spawn_split -a 'xdg-open {hint-url}' y",
 )
 config.bind(
     "Ft",
-    "hint links spawn qtile cmd-obj -o layout -f spawn_tab -a 'qb {hint-url}'",
+    "hint all spawn qtile cmd-obj -o layout -f spawn_tab -a 'xdg-open {hint-url}'",
 )
 config.bind(
     "FT",
-    "hint links spawn _qtile_spawn_new_tab '{hint-url}'",
+    "hint all spawn _qtile_spawn_new_tab '{hint-url}'",
 )
 config.bind(
     "Ff",
-    "hint links spawn qtile cmd-obj -o root -f spawn -a 'qb {hint-url}'",
+    "hint all spawn qtile cmd-obj -o root -f spawn -a 'xdg-open {hint-url}'",
 )
 
 # Medias
 config.bind("M", "hint all spawn mpv --x11-name='mpv-preview' {hint-url}")
 # Navigation
 config.bind("m", "mode-enter set_mark")
-
-# Settings
-config.bind("xb", "config-cycle statusbar.show never always")
-config.bind(
-    "xc",
-    "config-cycle content.user_stylesheets ~/.config/qutebrowser/css/default.css ~/.config/qutebrowser/css/custom.css",
-)
-config.bind("xt", "config-cycle tabs.show multiple always")
