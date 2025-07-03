8b451c61acff3d7e33e551ba2507edec27b6b27a
whammou@arch <khoabui.ld99@gmail.com>
2025-07-04 04:50:31 +0700

    new soft reset keys

--- fish/.config/fish/functions/_reset.fish
+++ fish/.config/fish/functions/_reset.fish
@@ -1,8 +1,4 @@
 function _reset
     reset
-    echo
-    #fastfetch
-    colorscript --random
-    echo \n
     commandline --function repaint
 end
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -4,14 +4,15 @@ function fish_user_key_bindings
     for mode in default insert
         bind --mode $mode \e\cp _paru_install
         bind --mode $mode \e\cq _paru_uninstall
-        bind --mode $mode \co _reset
+        bind --mode $mode \e\co _reset
+        bind --mode $mode \co _reset_color
         bind --mode $mode \cg _lazygit
 
         # bind --mode $mode \cZ true #unbind <C-z> send to background
         # bind --mode $mode \x1c bg
 
         bind --mode $mode \cf _yazi_current_token
-        bind --mode $mode \e\co _nvim_current_token
+        #bind --mode $mode \e\co _nvim_current_token
         bind --mode $mode \e\o _bat_preview_current_file
     end
 end
