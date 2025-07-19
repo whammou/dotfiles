c0af383c9bd580daa5edc9b1a13760f9b907aaa6
whammou@arch <khoabui.ld99@gmail.com>
2025-07-19 14:33:25 +0700

    tweak condig

--- nvim-lazy/.config/nvim/lua/config/extras/orgroam.lua
+++ nvim-lazy/.config/nvim/lua/config/extras/orgroam.lua
@@ -84,44 +84,6 @@ local task_doc_agenda = {
   },
 }
 
-local todo_keyword_faces = {
-  TODO = ":foreground #c75ae8 :weight bold :underline on",
-  DOING = ":foreground #34bfd0 :weight bold :underline on",
-  PENDING = ":foreground #93a4c3 :weight bold :underline on",
-  PARTIAL = ":foreground #efbd5d :weight bold :underline on",
-  ABORTED = ":foreground #f65866 :weight bold :underline on",
-  DONE = ":foreground #8bcd5b :weight bold :underline on",
-}
-
-local mappings = {
-  org_return_uses_meta_return = false,
-  global = {
-    org_capture = "<leader>oC",
-  },
-  org = {
-    org_cycle = false,
-    org_toggle_checkbox = "<Leader>oc",
-  },
-}
-
-local ui = {
-  input = {
-    use_vim_ui = true,
-  },
-  folds = {
-    colored = true,
-  },
-  agenda = {
-    preview_window = {
-      border = "single",
-    },
-  },
-}
-
-khalorg.setup({
-  calendar = "private",
-})
-
 roam.setup({
   directory = "~/orgroam/",
   org_files = { "~/orgroam/**/*", "~/notes/**/*" },
@@ -140,36 +102,8 @@ roam.setup({
 })
 
 orgmode.setup({
-  ui = ui,
-  mappings = mappings,
-  org_highlight_latex_and_related = "entities",
-
   org_custom_exports = custom_exports,
-  org_todo_keyword_faces = todo_keyword_faces,
   org_capture_templates = capture_templates,
-  org_agenda_files = { "~/notes/**/*" },
-  org_default_notes_file = "~/notes/capture.org",
-  org_archive_location = "./log.org::ARCHIVED",
-  org_log_into_drawer = "LOGBOOK",
-
-  org_ellipsis = "",
-  win_split_mode = "horizontal",
-
-  org_hide_leading_stars = false,
-  org_adapt_indentaion = false,
-  org_startup_indented = true,
-  org_id_link_to_org_use_id = true,
-  org_use_tag_inheritance = true,
-  org_tags_column = 0,
-  org_cycle_separator_lines = 0,
-  org_blank_before_new_entry = { heading = false, plain_list_item = false },
-
-  org_todo_keywords = { "TODO(t)", "DOING(d)", "(e)", "PENDING(p)", "|", "PARTIAL(r)", "DONE(f)", "ABORTED(a)" },
-  org_priority_highest = "A",
-  org_priority_default = "B",
-  org_priority_lowest = "F",
-  org_deadline_warning_days = 0,
-
   org_agenda_custom_commands = {
     k = {
       description = "Task Agenda",
--- nvim-lazy/.config/nvim/lua/plugins/extras/orgroam.lua
+++ nvim-lazy/.config/nvim/lua/plugins/extras/orgroam.lua
@@ -2,18 +2,75 @@ return {
   {
     "chipsenkbeil/org-roam.nvim",
     lazy = true,
-    ft = "org",
+    ft = { "org", "org-roam-node-buffer" },
     dependencies = {
       {
         "nvim-orgmode/orgmode",
         lazy = true,
-        config = function()
-          require("config.extras.orgroam")
-        end,
+        opts = {
+          org_agenda_files = { "~/notes/**/*" },
+          org_default_notes_file = "~/notes/capture.org",
+          org_archive_location = "./log.org::ARCHIVED",
+          org_log_into_drawer = "LOGBOOK",
+          org_highlight_latex_and_related = "entities",
+
+          org_return_uses_meta_return = false,
+          mappings = {
+            global = {
+              org_capture = "<leader>oC",
+            },
+            org = {
+              org_cycle = false,
+              org_toggle_checkbox = "<Leader>oc",
+            },
+          },
+
+          org_ellipsis = "",
+          win_split_mode = "horizontal",
+
+          org_hide_leading_stars = false,
+          org_adapt_indentaion = false,
+          org_startup_indented = true,
+          org_id_link_to_org_use_id = true,
+          org_use_tag_inheritance = true,
+          org_tags_column = 0,
+          org_cycle_separator_lines = 0,
+          org_blank_before_new_entry = { heading = false, plain_list_item = false },
+
+          org_priority_highest = "A",
+          org_priority_default = "B",
+          org_priority_lowest = "F",
+          org_deadline_warning_days = 0,
+
+          org_todo_keywords = { "TODO(t)", "DOING(d)", "(e)", "PENDING(p)", "|", "PARTIAL(r)", "DONE(f)", "ABORTED(a)" },
+          org_todo_keyword_faces = {
+            TODO = ":foreground #c75ae8 :weight bold :slant italic",
+            DOING = ":foreground #34bfd0 :weight bold :slant italic",
+            PENDING = ":foreground #93a4c3 :weight bold :slant italic",
+            PARTIAL = ":foreground #efbd5d :weight bold :slant italic",
+            ABORTED = ":foreground #f65866 :weight bold :slant italic",
+            DONE = ":foreground #8bcd5b :weight bold :slant italic",
+          },
+
+          input = {
+            use_vim_ui = true,
+          },
+          folds = {
+            colored = true,
+          },
+          agenda = {
+            preview_window = {
+              border = "single",
+            },
+          },
+        },
       },
       {
         "BartSte/nvim-khalorg",
         lazy = true,
+        opts = {
+          calendar = "private",
+        },
       },
       {
         "nvim-orgmode/org-bullets.nvim",
