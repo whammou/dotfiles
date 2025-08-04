7521d8ed8080603029f8173479cb4b72be1a492f
whammou@arch <khoabui.ld99@gmail.com>
2025-08-04 20:48:13 +0700

    added orgroam capture keybinds

--- qtile/.config/qtile/settings/keymaps.py
+++ qtile/.config/qtile/settings/keymaps.py
@@ -112,6 +112,7 @@ keymap = [
             ["a", in_terminal("orgmode-agenda"), "pad_large"],
             ["n", in_terminal("orgmode-notes"), "pad_large"],
             ["c", in_terminal("orgmode-capture"), "pad_large"],
+            ["S-c", in_terminal("orgroam-capture"), "pad_large"],
             # ["t", in_terminal("orgmode-todo"), "pad_large"],
         ],
     },
