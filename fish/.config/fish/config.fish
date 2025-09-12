1128a9f49a84f65fd7fd0e470a0c4ccbbd6f298d
whammou@arch <khoabui.ld99@gmail.com>
2025-09-12 08:49:29 +0700

    added nvim as pager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,20 @@
 if status is-interactive
-
-    # Commands to run in interactive sessions can go here
     starship init fish | source
-    # echo; neofetch --ascii_distro arch_small --ascii_colors 6 6 --colors 5 8 10 6 8 8
     echo
-    #fastfetch
     if not set -q NVIM
         colorscript --random
     end
     set fish_greeting
-
-    #    if string match -q -- '*' $TERM
-    #        set -g fish_vi_force_cursor 1
-    #    end
 end
+
 function fish_title
     set -q argv[1]; or set argv fish
     set -l current_path_abbreviated (fish_prompt_pwd_dir_length=1 prompt_pwd)
     set -l current_path_expanded (string replace '~' "HOME" -- "$current_path_abbreviated")
     echo "$current_path_expanded - "
 end
-#set fish_cursor_default block
-#set fish_cursor_insert line
+set fish_cursor_default block
+set fish_cursor_insert line
 
 # Term settings
 #export TERM=xterm-256color
