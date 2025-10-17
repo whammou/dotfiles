80313317bd3823eb7a8f709f982ca134ca1c70d0
whammou@arch <khoabui.ld99@gmail.com>
2025-10-17 21:59:29 +0700

    remove unbind scroll

--- qutebrowser/.config/qutebrowser/keys.py
+++ qutebrowser/.config/qutebrowser/keys.py
@@ -8,6 +8,8 @@ config = config  # noqa: F821 pylint: disable=E0602,C0103
 config.unbind("d")
 config.unbind("D")
 config.unbind("F")
+# config.unbind("j")
+# config.unbind("k")
 
 # Input method
 # config.bind("<Escape>", "spawn fcitx5-remote -s keyboard-us", mode="insert")
@@ -17,6 +19,8 @@ config.unbind("F")
 # Bind Escape in insert mode to switch Fcitx5 and then leave insert mode
 # This might require two presses or careful timing, as qutebrowser's own
 # Escape handling might still fire.config.bind('o', 'set statusbar.show always;; set-cmd-text -s :open')
+config.bind("<Control-e>", "scroll-px 0 34")
+config.bind("<Control-y>", "scroll-px 0 -34")
 config.bind(
     "<Escape>", "mode-enter normal;; set statusbar.show in-mode", mode="command"
 )
@@ -43,7 +47,7 @@ config.bind(
 config.bind("xb", "set statusbar.show never")
 config.bind(
     "xc",
-    "config-cycle content.user_stylesheets ~/.config/qutebrowser/css/default.css ~/.config/qutebrowser/css/custom-onedark.css",
+    "config-cycle content.user_stylesheets ~/.config/qutebrowser/css/onedark-app.css ~/.config/qutebrowser/css/custom-onedark.css",
 )
 config.bind("xt", "config-cycle tabs.show multiple always")
 # Qtile
@@ -61,6 +65,8 @@ config.bind("T", "set statusbar.show always ;; cmd-set-text -s :tab")
 config.bind("<Ctrl-T>", "set statusbar.show always ;; cmd-set-text -s :tab_new")
 
 config.bind("f", "set statusbar.show never ;; hint all")
+config.bind("FI", "set statusbar.show never ;; hint images spawn xdg-open {hint-url}")
+config.bind("Fi", "set statusbar.show never ;; hint inputs")
 config.bind(
     "Fv",
     "set statusbar.show never ;; hint all spawn qtile cmd-obj -o layout -f spawn_split -a 'xdg-open {hint-url}' x",
