5380583acb4cee24c3e6d733a3b901256a6ebda7
whammou@arch <khoabui.ld99@gmail.com>
2025-11-14 05:17:58 +0700

    changing theme to catppuccin

--- nvim-lazy/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim-lazy/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,77 +1,13 @@
 return {
   {
-    "navarasu/onedark.nvim",
+    "catppuccin/nvim",
     priority = 1000,
     lazy = true,
-    opts = {
-      style = "deep",
-      transparent = false,
-      term_colors = false,
-      lualine = {
-        transparent = false,
-      },
-      colors = {
-        dimmed_red = "#830712",
-        dimmed_green = "#406b20",
-        dimmed_yellow = "#8f610d",
-        dimmed_blue = "#024c8a",
-        dimmed_purple = "#661280",
-        dimmed_cyan = "#1b6a74",
-      },
-      highlights = {
-        -- Orgmode agenda
-        ["@org.agenda.day"] = { fg = "none", fmt = "bold" },
-        ["@org.agenda.today"] = { fg = "$yellow", fmt = "bold" },
-        ["@org.agenda.weekend"] = { fg = "$red", fmt = "bold" },
-        ["@org.agenda.deadline"] = { fg = "$cyan" },
-
-        -- Markdown headlines
-        ["@markup.heading.1.markdown"] = { fg = "$cyan" },
-        ["RenderMarkdownH1Bg"] = { bg = "$bg1" },
-        ["@markup.heading.2.markdown"] = { fg = "$purple" },
-        ["RenderMarkdownH2Bg"] = { bg = "$bg1" },
-        ["@markup.heading.3.markdown"] = { fg = "$blue" },
-        ["RenderMarkdownH3Bg"] = { bg = "$bg1" },
-        ["@markup.heading.4.markdown"] = { fg = "$yellow" },
-        ["RenderMarkdownH4Bg"] = { bg = "$bg1" },
-        ["@markup.heading.5.markdown"] = { fg = "$green" },
-        ["RenderMarkdownH5Bg"] = { bg = "$bg1" },
-        ["@markup.heading.6.markdown"] = { fg = "$red" },
-        ["RenderMarkdownH6Bg"] = { bg = "$bg1" },
-
-        -- Orgmode headlines
-        ["@org.tag.org"] = { fg = "$grey", fmt = "italic" },
-
-        ["@org.headline.level1"] = { fg = "$cyan", fmt = "bold" },
-        ["@org.headline.level2"] = { fg = "$purple", fmt = "bold" },
-        ["@org.headline.level3"] = { fg = "$blue", fmt = "bold" },
-        ["@org.headline.level4"] = { fg = "$yellow", fmt = "bold" },
-        ["@org.headline.level5"] = { fg = "$green", fmt = "bold" },
-        ["@org.headline.level6"] = { fg = "$red", fmt = "bold" },
-
-        --Snack dashboard
-        ["SnacksDashBoardHeader"] = { fg = "$fg" },
-        ["SnacksDashBoardFooter"] = { fg = "$fg" },
-        ["SnacksDashBoardSpecial"] = { fg = "$fg" },
-        ["SnacksDashBoardDesc"] = { fg = "$fg" },
-        ["SnacksDashBoardIcon"] = { fg = "$fg" },
-        ["SnacksDashBoardKey"] = { fg = "$fg" },
-
-        ["Conceal"] = { bg = "$bg0", fg = "$red" },
-
-        ["WhichKeyDesc"] = { fg = "$green" },
-        ["WhichKeySeparator"] = { fg = "$yellow" },
-
-        ["FzfLuaBorder"] = { fg = "$grey" },
-        ["FzfLuaFzfPrompt"] = { fg = "$blue" },
-      },
-    },
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,9 +1,25 @@
 # pylint: disable=C0111
+import os
+from urllib.request import urlopen
+
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
+# load your autoconfig, use this, if the rest of your config is empty!
+
+config.load_autoconfig()
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+
+    theme.setup(c, "macchiato", True)
 
 
-config.source("./themes/onedark.py")
 config.source("./keys.py")
 config.source("./chromium.py")
 config.source("./settings.py")
