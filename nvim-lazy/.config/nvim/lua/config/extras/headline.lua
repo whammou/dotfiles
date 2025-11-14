f574695cc1142bff1c7b90abf43314ab438e08ef
whammou@arch <khoabui.ld99@gmail.com>
2025-11-14 22:12:30 +0700

    system: catpuccinn preview

--- nvim-lazy/.config/nvim/lua/config/extras/headline.lua
+++ nvim-lazy/.config/nvim/lua/config/extras/headline.lua
@@ -1,8 +1,3 @@
-vim.cmd([[highlight Headline1 guibg=#1e2718]])
-vim.cmd([[highlight Headline2 guibg=#21262d]])
-vim.cmd([[highlight CodeBlock guibg=#1c1c1c]])
-vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
-
 require("headlines").setup({
   markdown = {
     headline_highlights = false,
@@ -13,12 +8,7 @@ require("headlines").setup({
   },
   org = {
     headline_highlights = {
-      "@org.headline.level1.org",
-      "@org.headline.level2.org",
-      "@org.headline.level3.org",
-      "@org.headline.level4.org",
-      "@org.headline.level5.org",
-      "@org.headline.level6.org",
+      "ColorColumn",
     },
   },
 })
