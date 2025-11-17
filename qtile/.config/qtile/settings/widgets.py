7fd1b8ced2794accb9c7da5d72b476f2f238f5d5
whammou@arch <khoabui.ld99@gmail.com>
2025-11-18 01:30:17 +0700

    qtile: update icons

--- qtile/.config/qtile/settings/widgets.py
+++ qtile/.config/qtile/settings/widgets.py
@@ -58,8 +58,8 @@ def checkupdate(command="checkupdates"):
         background=colors["bg2"],
         colour_have_updates=colors["yellow"],
         colour_no_updates=colors["yellow"],
-        display_format=" 󰳿 PKGS: {updates} ",
-        no_update_string=" 󰪩 PKGS: 0 ",
+        display_format=" 󱧘 PKGS: {updates} ",
+        no_update_string=" 󰏗 PKGS: 0 ",
         update_interval=1800,
         custom_command=command,
     )
@@ -108,7 +108,7 @@ def net():
 def wlan():
     return widget.Wlan(
         **base(bg="bg2", fg="purple"),
-        format=" 󰤨 STRG: {percent:2.0%} ",
+        format=" 󰢾 SIGN: {percent:2.0%} ",
         update_interval=60,
     )
 
