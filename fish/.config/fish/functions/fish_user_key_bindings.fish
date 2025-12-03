f96442559e12d9c244a3c9ca95b34e89a5f24479
whammou@arch <khoabui.ld99@gmail.com>
2025-12-03 12:35:46 +0700

    fish: keybinds

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
     fish_vi_key_bindings default
 
     for mode in default insert
-        bind --mode $mode \e\cp _paru_install
+        bind --mode $mode \e\cu _paru_install
         bind --mode $mode \e\cq _paru_uninstall
         bind --mode $mode \e\co _reset
         bind --mode $mode \co _reset_color
