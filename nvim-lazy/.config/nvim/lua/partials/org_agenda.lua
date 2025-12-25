800ab0b65e9c95b666b71ef042bf39989d5d7a4d
whammou@arch <khoabui.ld99@gmail.com>
2025-12-25 23:09:32 +0700

    nvim-orgmode: modify orgmode-agenda script

--- nvim-lazy/.config/nvim/lua/partials/org_agenda.lua
+++ nvim-lazy/.config/nvim/lua/partials/org_agenda.lua
@@ -3,8 +3,9 @@ local Snacks = require("snacks")
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "orgagenda",
   callback = function()
-    Snacks.zen.zoom()
+    --Snacks.zen.zoom()
     Snacks.bufdelete.other()
-    vim.opt.spell = false
+    vim.cmd.only()
+    --vim.opt.spell = false
   end,
 })
