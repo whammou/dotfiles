d114b8a26faf026cc0620389bb1fef31856dec20
whammou@arch <khoabui.ld99@gmail.com>
2026-02-05 23:39:50 +0700

    yazi: remove gfvs package

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,5 +1,19 @@
+local orange = "#DD9046"
+
+th.git = th.git or {}
+th.git.modified = ui.Style():fg(orange):bold() --orange
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.added = ui.Style():fg("green"):bold()
+th.git = th.git or {}
+th.git.modified_sign = " "
+th.git.deleted_sign = " "
+th.git.added_sign = " "
+
+require("git"):setup({
+	order = 1500,
+})
+
 require("starship"):setup()
-require("git"):setup()
 require("mime-ext"):setup({
 	with_exts = {
 		org = "text/org",
@@ -9,46 +23,12 @@ require("mime-ext"):setup({
 	},
 })
 
-require("gvfs"):setup({
-	-- (Optional) Allowed keys to select device.
-	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
-
-	-- (Optional) Table of blacklisted devices. These devices will be ignored in any actions
-	-- List of device properties to match, or a string to match the device name:
-	-- https://github.com/boydaihungst/gvfs.yazi/blob/master/main.lua#L144
-	blacklist_devices = { { name = "Wireless Device", scheme = "mtp" }, { scheme = "file" }, "Device Name" },
-
-	-- (Optional) Save file.
-	-- Default: ~/.config/yazi/gvfs.private
-	save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
-
-	-- (Optional) Input box position.
-	-- Default: { "top-center", y = 3, w = 60 },
-	-- Position, which is a table:
-	-- 	`1`: Origin position, available values: "top-left", "top-center", "top-right",
-	-- 	     "bottom-left", "bottom-center", "bottom-right", "center", and "hovered".
-	--         "hovered" is the position of hovered file/folder
-	-- 	`x`: X offset from the origin position.
-	-- 	`y`: Y offset from the origin position.
-	-- 	`w`: Width of the input.
-	-- 	`h`: Height of the input.
-	input_position = { "center", y = 0, w = 60 },
-
-	-- (Optional) Select where to save passwords.
-	-- Default: nil
-	-- Available options: "keyring", "pass", or nil
-	password_vault = "keyring",
-
-	-- (Optional) Only need if you set password_vault = "pass"
-	-- Read the guide at SECURE_SAVED_PASSWORD.md to get your key_grip
-	key_grip = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-
-	-- (Optional) Auto-save password after mount.
-	-- Default: false
-	save_password_autoconfirm = true,
-	-- (Optional) mountpoint of gvfs. Default: /run/user/USER_ID/gvfs
-	-- On some system it could be ~/.gvfs
-	-- You can't decide this path, it will be created automatically. Only changed if you know where gvfs mountpoint is.
-	-- Use command `ps aux | grep gvfs` to search for gvfs process and get the mountpoint path.
-	-- root_mountpoint = (os.getenv("XDG_RUNTIME_DIR") or ("/run/user/" .. ya.uid())) .. "/gvfs"
-})
+--require("gvfs"):setup({
+--	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
+--	blacklist_devices = { { name = "Wireless Device", scheme = "mtp" }, { scheme = "file" }, "Device Name" },
+--	save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
+--	input_position = { "center", y = 0, w = 60 },
+--	password_vault = "keyring",
+--	key_grip = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+--	save_password_autoconfirm = true,
+--})
