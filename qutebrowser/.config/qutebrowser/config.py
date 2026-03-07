f2f20e334c3abe79428a1e460756dd7860d00aff
whammou@arch <khoabui.ld99@gmail.com>
2026-03-08 03:23:22 +0700

    qutebrowser: update css rules

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,7 +2,6 @@
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
-
 config.source("./themes/onedark.py")
 config.source("./keys.py")
 config.source("./chromium.py")
