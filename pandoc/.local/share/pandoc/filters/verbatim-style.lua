026f11e38ed7588d5c56407b9c7349ebc8554e3c
whammou@arch <khoabui.ld99@gmail.com>
2026-05-30 00:24:52 +0700

    feat(pandoc-orgmode): avoiding orphan headlines

--- pandoc/.local/share/pandoc/filters/verbatim-style.lua
+++ pandoc/.local/share/pandoc/filters/verbatim-style.lua
@@ -1,3 +1,18 @@
+local function escape_latex(text)
+  -- backslash first: must escape \ before other chars it prefixes
+  text = text:gsub("\\", "\\textbackslash{}")
+  text = text:gsub("{", "\\{")
+  text = text:gsub("}", "\\}")
+  text = text:gsub("%%", "\\%%")
+  text = text:gsub("%$", "\\$")
+  text = text:gsub("&", "\\&")
+  text = text:gsub("#", "\\#")
+  text = text:gsub("_", "\\_")
+  text = text:gsub("~", "\\textasciitilde{}")
+  text = text:gsub("%^", "\\textasciicircum{}")
+  return text
+end
+
 function Code(elem)
-  return pandoc.RawInline('latex', '\\code{' .. elem.text .. '}')
+  return pandoc.RawInline('latex', '\\code{' .. escape_latex(elem.text) .. '}')
 end
