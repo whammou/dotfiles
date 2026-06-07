1b607ab27da88b6709952666d379229bd6bcf7b7
whammou@arch <khoabui.ld99@gmail.com>
2026-06-08 05:53:54 +0700

    feat(kitty): better temrinal movement

--- nvim-lazy/.config/nvim/lua/config/keymaps/window.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps/window.lua
@@ -1,10 +1,14 @@
 local map = vim.keymap.set
 
 for i = 1, 9 do
-  map({ "n", "t" }, "<leader>" .. i, function()
-    vim.api.nvim_set_current_win(vim.api.nvim_list_wins()[i])
+  map({ "n", "t" }, "<M-S-" .. i .. ">", function()
+    local wins = vim.api.nvim_list_wins()
+    if wins[i] then
+      vim.api.nvim_set_current_win(wins[i])
+    end
   end, { desc = "which_key_ignore" })
 end
 
 map("n", "<leader>ws", "<cmd>split # | wincmd p<CR>", { desc = "Split with prev buffer, keep focus" })
 map("n", "<leader>wv", "<cmd>vsplit # | wincmd p<CR>", { desc = "Vsplit with prev buffer, keep focus" })
+
