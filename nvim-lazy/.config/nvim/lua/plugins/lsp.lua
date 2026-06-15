886b7df572979f55888d478db417b3223e561ddb
whammou@arch <khoabui.ld99@gmail.com>
2026-06-15 10:34:13 +0700

    feat(nvim): added lsp config

--- nvim-lazy/.config/nvim/lua/plugins/lsp.lua
+++ nvim-lazy/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,32 @@ return {
       folds = {
         enabled = true, -- prevents LazyVim's foldingRange handler from overriding orgmode's foldmethod
       },
+      servers = {
+        pyright = {
+          handlers = {
+            ["$/progress"] = function(err, result, ctx)
+              if result and result.token then
+                if result.token == (vim.g.pyright_progress_token or result.token) then
+                  vim.g.pyright_progress_token = result.token
+                  vim.lsp.handlers["$/progress"](err, result, ctx)
+                end
+              end
+            end,
+          },
+        },
+        pylsp = {
+          handlers = {
+            ["$/progress"] = function(err, result, ctx)
+              if result and result.token then
+                if result.token == (vim.g.pylsp_progress_token or result.token) then
+                  vim.g.pylsp_progress_token = result.token
+                  vim.lsp.handlers["$/progress"](err, result, ctx)
+                end
+              end
+            end,
+          },
+        },
+      },
     },
   },
 }
