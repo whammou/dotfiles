83b58086d5aaa30ee52341a84827ba5a934ff3a6
whammou@arch <khoabui.ld99@gmail.com>
2026-06-15 10:35:31 +0700

    update(fish, yazi): packages update

--- fish/.config/fish/functions/fman.fish
+++ fish/.config/fish/functions/fman.fish
@@ -5,6 +5,7 @@ function fman --description "Fuzzy-find and preview man pages"
     end
 
     man -k . | fzf -q $query --prompt='man> ' \
-        --preview "echo {} | tr -d '()' | awk '{printf \"%s \", \$2} {print \$1}' | MANROFFOPT=\"-W font\" xargs -r man | col -bx | bat -l man -p --color always" |
-        tr -d '()' | awk '{printf "%s ", $2} {print $1}' | MANROFFOPT="-W font" xargs -r man
+        --preview "echo {} | tr -d '()' | awk '{printf \"%s \", \$2} {print \$1}' | xargs -r man 2>/dev/null | col -bx | bat -l man -p --color always" |
+        tr -d '()' | awk '{printf "%s ", $2} {print $1}' | xargs -r man 2>/dev/null
+    commandline --function repaint
 end
