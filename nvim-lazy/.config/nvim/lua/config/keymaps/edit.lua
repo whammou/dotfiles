8f1943c0dfa0242970ac87b7fe610d67424434dc
whammou@arch <khoabui.ld99@gmail.com>
2026-07-06 00:22:44 +0700

    feat(nvim): create new line above and below in insert-mode

--- nvim-lazy/.config/nvim/lua/config/keymaps/edit.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps/edit.lua
@@ -3,3 +3,5 @@ local map = vim.keymap.set
 map("n", "<C-g>u", "<cmd>GetCurrentBranchLink<CR>", { desc = "Get current branch link" })
 map("n", "<C-A-j>", "<cmd>set paste<CR>m`o<ESC>``<cmd>set nopaste<CR>", { desc = "Add empty line above" })
 map("n", "<C-A-k>", "<cmd>set paste<CR>m`O<ESC>``<cmd>set nopaste<CR>", { desc = "Add empty line below" })
+map("i", "<C-A-j>", "<Escape><cmd>set paste<CR>m`o<ESC>``<cmd>set nopaste<CR>i", { desc = "Add empty line above" })
+map("i", "<C-A-k>", "<Escape><cmd>set paste<CR>m`O<ESC>``<cmd>set nopaste<CR>i", { desc = "Add empty line below" })
